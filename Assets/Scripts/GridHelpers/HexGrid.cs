ddd620ec865923b77ce53886ed565230fc660618
Feeziks <feeziksg@gmail.com>
2021-09-28 22:55:41 -0600

    Blending of colors tutorial done. Had to do a lot of ctrl+c ctrl+v engineering since I could not figure out a small bug

--- Assets/Scripts/GridHelpers/HexGrid.cs
+++ Assets/Scripts/GridHelpers/HexGrid.cs
@@ -1,121 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-namespace UnityHelpers
-{
-  public class HexGrid : MonoBehaviour
-  {
-    #region Properties / Fields
+public class HexGrid : MonoBehaviour {
 
-    public int width;
-    public int height;
+	public int width = 6;
+	public int height = 6;
 
-    public HexCell cellPrefab;
-    public TextMeshProUGUI cellLabelPrefab;
-    public HexOrientation orientation;
+	public Color defaultColor = Color.white;
 
-    public Color defaultColor;
-    public Color selectedColor;
+	public HexCell cellPrefab;
+	public TextMeshProUGUI cellLabelPrefab;
 
-    private HexMesh hexMesh { get; set; }
-    private Canvas gridCanvas { get; set; }
-    private HexCell[] cells { get; set; }
+	HexCell[] cells;
 
-    #endregion
+	Canvas gridCanvas;
+	HexMesh hexMesh;
 
-    #region Unity Methods
+	void Awake () {
+		gridCanvas = GetComponentInChildren<Canvas>();
+		hexMesh = GetComponentInChildren<HexMesh>();
 
-    private void Awake()
-    {
-      gridCanvas = GetComponentInChildren<Canvas>();
-      hexMesh = GetComponentInChildren<HexMesh>();
-      hexMesh.SetOrientation(orientation);
+		cells = new HexCell[height * width];
 
-      cells = new HexCell[height * width];
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
 
-      for (int z = 0, i = 0; z < height; z++)
-      {
-        for (int x = 0; x < width; x++)
-        {
-          CreateCell(x, z, i++);
-        }
-      }
-    }
+	void Start () {
+		hexMesh.Triangulate(cells);
+	}
 
-    private void Start()
-    {
-      hexMesh.Triangulate(cells);
-    }
+	public void ColorCell (Vector3 position, Color color) {
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = color;
+		hexMesh.Triangulate(cells);
+	}
 
-    private void Update()
-    {
-      if(Input.GetMouseButton(0))
-      {
-        HandleInput();
-      }
-    }
+	void CreateCell (int x, int z, int i) {
+		Vector3 position;
+		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+		position.y = 0f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f);
 
-    #endregion
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
 
-    #region Public Methods
+		if (x > 0) {
+			cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+		}
+		if (z > 0) {
+			if ((z & 1) == 0) {
+				cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+				if (x > 0) {
+					cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+				}
+			}
+			else {
+				cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+				if (x < width - 1) {
+					cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+				}
+			}
+		}
 
-    #endregion
-
-    #region Private Methods
-
-    private void CreateCell(int x, int z, int i)
-    {
-      Vector3 position;
-      position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
-      position.y = 0f;
-      position.z = z * (HexMetrics.outerRadius * 1.5f);
-
-      HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
-      cell.transform.SetParent(transform, false);
-      cell.transform.localPosition = position;
-      cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
-      cell.color = defaultColor;
-
-      LabelCell(position.x, position.z, x, z, cell);
-    }
-
-    private void LabelCell(float xPos, float zPos, int xIdx, int zIdx, HexCell cell)
-    {
-      TextMeshProUGUI label = Instantiate(cellLabelPrefab);
-      label.rectTransform.SetParent(gridCanvas.transform, false);
-      label.rectTransform.anchoredPosition = new Vector2(xPos, zPos);
-      label.text = cell.coordinates.ToStringLineBreak();
-    }
-
-    private void HandleInput()
-    {
-      Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-      RaycastHit hit;
-      if(Physics.Raycast(inputRay, out hit))
-      {
-        TouchCell(hit.point);
-      }
-    }
-
-    private void TouchCell(Vector3 position)
-    {
-      position = transform.InverseTransformPoint(position);
-      HexCoordinates coordinates = HexCoordinates.FromPosition(position);
-      HexCell touchedCell = HexCoordinatesToCell(coordinates);
-      touchedCell.color = selectedColor;
-      hexMesh.Triangulate(cells);
-      Debug.Log("Touched at " + coordinates.ToString());
-    }
-
-    private HexCell HexCoordinatesToCell(HexCoordinates coordinates)
-    {
-      int index = coordinates.x + coordinates.z * width + coordinates.z / 2;
-      return cells[index];
-    }
-
-    #endregion
-  }
+		TextMeshProUGUI label = Instantiate<TextMeshProUGUI>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = cell.coordinates.ToStringOnSeparateLines();
+	}
 }
--- Assets/Scripts/GridHelpers/HexMesh.cs
+++ Assets/Scripts/GridHelpers/HexMesh.cs
@@ -1,96 +1,128 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
-namespace UnityHelpers
-{
-  [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-  public class HexMesh : MonoBehaviour
-  {
-    #region Properties / Fields
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour {
 
-    Mesh hexMesh;
-    List<Vector3> vertices;
-    List<int> triangles;
-    List<Color> colors;
-    HexOrientation orientation;
-    MeshCollider meshCollider;
+	Mesh hexMesh;
+	List<Vector3> vertices;
+	List<Color> colors;
+	List<int> triangles;
 
-    #endregion
+	MeshCollider meshCollider;
 
-    #region Unity Methods
+	void Awake () {
+		GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+		meshCollider = gameObject.AddComponent<MeshCollider>();
+		hexMesh.name = "Hex Mesh";
+		vertices = new List<Vector3>();
+		colors = new List<Color>();
+		triangles = new List<int>();
+	}
 
-    private void Awake()
-    {
-      GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
-      meshCollider = gameObject.AddComponent<MeshCollider>();
-      hexMesh.name = "Hex mesh";
-      vertices = new List<Vector3>();
-      triangles = new List<int>();
-      colors = new List<Color>();
-    }
+	public void Triangulate (HexCell[] cells) {
+		hexMesh.Clear();
+		vertices.Clear();
+		colors.Clear();
+		triangles.Clear();
+		for (int i = 0; i < cells.Length; i++) {
+			Triangulate(cells[i]);
+		}
+		hexMesh.vertices = vertices.ToArray();
+		hexMesh.colors = colors.ToArray();
+		hexMesh.triangles = triangles.ToArray();
+		hexMesh.RecalculateNormals();
+		meshCollider.sharedMesh = hexMesh;
+	}
 
-    #endregion
+	void Triangulate (HexCell cell) {
+		for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+			Triangulate(d, cell);
+		}
+	}
 
-    #region Public Methods
+	void Triangulate (HexDirection direction, HexCell cell) {
+		Vector3 center = cell.transform.localPosition;
+		Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+		Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
 
-    public void Triangulate(HexCell[] cells)
-    {
-      hexMesh.Clear();
-      vertices.Clear();
-      triangles.Clear();
-      colors.Clear();
+		AddTriangle(center, v1, v2);
+		AddTriangleColor(cell.color);
 
-      for(int i = 0; i < cells.Length; i++)
-      {
-        Triangulate(cells[i]);
-      }
+		if (direction <= HexDirection.SE) {
+			TriangulateConnection(direction, cell, v1, v2);
+		}
+	}
 
-      hexMesh.vertices = vertices.ToArray();
-      hexMesh.triangles = triangles.ToArray();
-      hexMesh.colors = colors.ToArray();
-      hexMesh.RecalculateNormals();
+	void TriangulateConnection (
+		HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2
+	) {
+		HexCell neighbor = cell.GetNeighbor(direction);
+		if (neighbor == null) {
+			return;
+		}
 
-      meshCollider.sharedMesh = hexMesh;
-    }
+		Vector3 bridge = HexMetrics.GetBridge(direction);
+		Vector3 v3 = v1 + bridge;
+		Vector3 v4 = v2 + bridge;
 
-    public void SetOrientation(HexOrientation o)
-    {
-      orientation = o;
-    }
+		AddQuad(v1, v2, v3, v4);
+		AddQuadColor(cell.color, neighbor.color);
 
-    #endregion
+		HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+		if (direction <= HexDirection.E && nextNeighbor != null) {
+			AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
+			AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
+		}
+	}
 
-    #region Private Methods
+	void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+	}
 
-    private void Triangulate (HexCell cell)
-    {
-      Vector3 center = cell.transform.localPosition;
-      for(int i = 0; i < 6; i++)
-      {
-        AddTriangle(center, center + HexMetrics.corners[orientation][i], center + HexMetrics.corners[orientation][i + 1]);
-        AddTriangleColor(cell.color);
-      }
-    }
+	void AddTriangleColor (Color color) {
+		colors.Add(color);
+		colors.Add(color);
+		colors.Add(color);
+	}
 
-    private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
-    {
-      int vertexIndex = vertices.Count;
-      vertices.Add(v1);
-      vertices.Add(v2);
-      vertices.Add(v3);
-      triangles.Add(vertexIndex);
-      triangles.Add(vertexIndex + 1);
-      triangles.Add(vertexIndex + 2);
-    }
+	void AddTriangleColor (Color c1, Color c2, Color c3) {
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+	}
 
-    private void AddTriangleColor(Color c)
-    {
-      colors.Add(c);
-      colors.Add(c);
-      colors.Add(c);
-    }
+	void AddQuad (Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4) {
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		vertices.Add(v4);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 3);
+	}
 
-    #endregion
-  }
+	void AddQuadColor (Color c1, Color c2) {
+		colors.Add(c1);
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c2);
+	}
+
+	void AddQuadColor (Color c1, Color c2, Color c3, Color c4) {
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+		colors.Add(c4);
+	}
 }
